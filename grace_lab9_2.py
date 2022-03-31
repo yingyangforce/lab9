4bca654686d8f372ccac2467d3a94a11c0262930
Gene Calmer <yingyangforce@gmail.com>
2022-03-31 01:30:12 -0400

    populate description

---
+++ grace_lab9_2.py
@@ -0,0 +1,7 @@
+#+--------------------------+
+#| lab9_2.py - Isaiah Grace |
+#+--------------------------+
+
+# Task 2 - Given a board length, find into how many 4ft sections it can be cut.
+#          (Use recursion!)
+
